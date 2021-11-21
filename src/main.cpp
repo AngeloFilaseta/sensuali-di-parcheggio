ae31d04bb96dbfccf1e9e4af4b6e73084bb16e38
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-11-21 16:33:47 +0100

    mp3 aduio player working with an example

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,96 @@
-#include <Arduino.h>
-#include <SoftwareSerial.h>
+#include "Arduino.h"
 
-#include <hw/Potentiometer.h>
-#include <hw/Sonar.h>
-#include "../.pio/libdeps/esp12e/DFPlayerMini_Fast/src/DFPlayerMini_Fast.h"
+#include "../.pio/libdeps/esp12e/DFRobotDFPlayerMini/DFRobotDFPlayerMini.h"
+#include "SoftwareSerial.h"
 
-//PINS
-#define POTENTIOMETER_PIN A0
+#define RX_PIN D5
+#define TX_PIN D6
 
-#define SONAR_TRIG_PIN 5
-#define SONAR_ECHO_PIN 4
+SoftwareSerial softwareSerial(RX_PIN, TX_PIN); // RX, TX
+DFRobotDFPlayerMini mp3Player;
 
-#define RX_PIN 10
-#define TX_PIN 11
+void printDetail(uint8_t type, int value);
 
-//CONSTANTS
-#define MAX_VOLUME 30
-
-SoftwareSerial mp3Serial(RX_PIN, TX_PIN); // RX, TX
-DFPlayerMini_Fast mp3;
-
-Potentiometer* potentiometer = new Potentiometer(POTENTIOMETER_PIN);
-Sonar* sonar = new Sonar(SONAR_TRIG_PIN, SONAR_ECHO_PIN);
-
-void setup() {
-    pinMode(SONAR_TRIG_PIN, OUTPUT);
-    pinMode(SONAR_ECHO_PIN, INPUT);
+[[noreturn]] void setup()
+{
+    softwareSerial.begin(9600);
     Serial.begin(9600);
-    mp3Serial.begin(9600);
-    mp3.begin(mp3Serial, true);
-    mp3.volume(30);
-    mp3.loop(1);
+
+    Serial.println();
+    Serial.println(F("DFRobot DFPlayer Mini Demo"));
+    Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+    if (!mp3Player.begin(softwareSerial)) {  //Use softwareSerial to communicate with mp3.
+        Serial.println(F("Unable to begin:"));
+        Serial.println(F("1.Please recheck the connection!"));
+        Serial.println(F("2.Please insert the SD card!"));
+        while(true);
+    }
+    Serial.println(F("DFPlayer Mini online."));
+
+    mp3Player.volume(10);  //Set volume value. From 0 to 30
+    mp3Player.play(1);  //Play the first mp3
 }
 
-void setVolume(){
-    int newVolume = (int)map(potentiometer->value(), 0, MAX_POTENTIOMETER_VALUE, 0, MAX_VOLUME);
-    mp3.volume(newVolume);
-}
-
-void playAudio(){
-    if(!mp3.isPlaying()){
-        int distance = sonar->distance();
-        int trackToPlay;
-        if(distance > 10) {
-            trackToPlay = 1;
-        } else {
-            trackToPlay = 2;
-        }
-        mp3.play(trackToPlay);
+void loop()
+{
+    if (mp3Player.available()) {
+        printDetail(mp3Player.readType(), mp3Player.read()); //Print the detail message from DFPlayer to handle different errors and states.
     }
 }
 
-void loop() {
-    //setVolume();
-    //playAudio();
-    //Serial.println((String)"Potentiometer: " + potentiometer->value());
-    //Serial.println((String)"Sonar: " + sonar->distance());
-    //delay(1000);
+void printDetail(uint8_t type, int value){
+    switch (type) {
+        case TimeOut:
+            Serial.println(F("Time Out!"));
+            break;
+        case WrongStack:
+            Serial.println(F("Stack Wrong!"));
+            break;
+        case DFPlayerCardInserted:
+            Serial.println(F("Card Inserted!"));
+            break;
+        case DFPlayerCardRemoved:
+            Serial.println(F("Card Removed!"));
+            break;
+        case DFPlayerCardOnline:
+            Serial.println(F("Card Online!"));
+            break;
+        case DFPlayerPlayFinished:
+            Serial.print(F("Number:"));
+            Serial.print(value);
+            Serial.println(F(" Play Finished!"));
+            break;
+        case DFPlayerError:
+            Serial.print(F("DFPlayerError:"));
+            switch (value) {
+                case Busy:
+                    Serial.println(F("Card not found"));
+                    break;
+                case Sleeping:
+                    Serial.println(F("Sleeping"));
+                    break;
+                case SerialWrongStack:
+                    Serial.println(F("Get Wrong Stack"));
+                    break;
+                case CheckSumNotMatch:
+                    Serial.println(F("Check Sum Not Match"));
+                    break;
+                case FileIndexOut:
+                    Serial.println(F("File Index Out of Bound"));
+                    break;
+                case FileMismatch:
+                    Serial.println(F("Cannot Find File"));
+                    break;
+                case Advertise:
+                    Serial.println(F("In Advertise"));
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
+
 }
-
-

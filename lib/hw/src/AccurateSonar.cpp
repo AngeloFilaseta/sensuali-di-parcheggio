d5bcd0f508a54d96fc773728b196f01f0782d8f1
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-18 13:24:05 +0100

    AccurateSensor is now more accurate, added docs

--- lib/hw/src/AccurateSonar.cpp
+++ lib/hw/src/AccurateSonar.cpp
@@ -2,19 +2,24 @@
 #include "AccurateSonar.h"
 
 AccurateSonar::AccurateSonar(char trigPin, char echoPin) : HwComponent(trigPin) {
-    this -> echoPin = echoPin;
+    _echoPin = echoPin;
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
 }
 
 int AccurateSonar::distance() {
-    long unsigned distance[REP];
-    int sum = 0;
-    for(int i = 0; i < REP; i++){
-        distance[i] = ((SOUND_SPEED/100) * singleRead()) /20000;
-        sum += distance[i];
+    int distance[REP];
+    long sum = 0;
+    char elements = REP;
+    for(int i = 0; i < REP; i++) {
+        distance[i] = (SOUND_SPEED * (int) singleRead()) / 20000;
+        if(distance[i] >= 0) {
+            sum += distance[i];
+        } else {
+            elements--;
+        }
     }
-    return (sum/REP)/100;
+    return sum / elements;
 }
 
 unsigned long AccurateSonar::singleRead() {
@@ -22,5 +27,5 @@ unsigned long AccurateSonar::singleRead() {
     delayMicroseconds(3);
     digitalWrite(pin(), HIGH);
     delayMicroseconds(12);
-    return(pulseIn(this -> echoPin, HIGH, 35000)); // microseconds of (total) sound travel
+    return(pulseIn(_echoPin, HIGH, 35000)); // microseconds of (total) sound travel
 }
--- lib/hw/src/AccurateSonar.h
+++ lib/hw/src/AccurateSonar.h
@@ -4,20 +4,36 @@
 #include "HwComponent.h"
 
 #define REP 20 // Number of readings done for a single distance() call
-#define SOUND_SPEED 331300 + 606 * 20 // 20 is the temperature of the air in C°.
+#define SOUND_SPEED 66381 // 20 is the temperature of the air in C°.
 
+/**
+ * @brief Sonar class that executes serveral reads and return an average value.
+ */
 class AccurateSonar : public HwComponent {
 public:
 
+    /**
+     * @brief Construct a new Accurate Sonar object.
+     * 
+     * @param trigPin the trigger pin.
+     * @param echoPin the echo pin.
+     */
     AccurateSonar(char trigPin, char echoPin);
 
-    //returns the distance (mm) detected by the sonar
+    /**
+     * @brief returns the distance (mm) detected by the Sonar.
+     */
     int distance();
 
 private:
 
-    char echoPin;
+    char _echoPin;
 
+    /**
+     * @brief The single read executed by the Sonar.
+     * 
+     * @return unsigned long the value read.
+     */
     unsigned long singleRead();
 
 };

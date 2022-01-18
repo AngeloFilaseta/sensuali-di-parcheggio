66db51cd37e45a8f7802a2b3938cd4fadfe03237
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-01-18 12:10:44 +0100

    removed potentiometer

--- lib/hw/src/Potentiometer.cpp
+++
@@ -1,8 +0,0 @@
-#include "Potentiometer.h"
-#include <Arduino.h>
-
-Potentiometer::Potentiometer(char pin) : HwComponent(pin) { }
-
-int Potentiometer::value() {
-    return analogRead(this -> pin());
-}
